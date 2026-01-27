2dd8afcb14bd5a8262e363c23d0bd13dc6f565b6
wgeesey <89809942+wgeesey@users.noreply.github.com>
2026-01-27 15:49:34 +0000

    Add files via upload

---
+++ Spark/S3_Data_Lake.py
@@ -0,0 +1,56 @@
+import argparse
+from email import header
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+
+# Arg Parser to read input arguments from command line/terminal
+parser = argparse.ArgumentParser(description='Most popular listings parameters')
+parser.add_argument('--listings', help='Path to the listings dataset') # --listings command-line argument is stored in args.listings
+parser.add_argument('--reviews', help='Path to the reviews dataset')
+parser.add_argument('--output', help='Directory to save the output')
+args = parser.parse_args()
+
+# Start Spark Session
+spark = SparkSession.builder \
+    .appName("Most popular listings") \
+    .getOrCreate()
+
+# Read the data sets in
+listings = spark.read.csv(args.listings,
+    header=True,
+    inferSchema=True,
+    sep=",",
+    quote='"',
+    escape='"',
+    multiLine=True,
+    mode="PERMISSIVE"
+)
+
+reviews = spark.read.csv(args.reviews,
+    header=True,
+    inferSchema=True,
+    sep=",",
+    quote='"',
+    escape='"',
+    multiLine=True,
+    mode="PERMISSIVE"
+)
+
+
+# Join the data sets together
+listings_reviews = listings.join(
+    reviews, listings.id == reviews.listing_id, how='inner'
+)
+
+# Perform a GroupBy
+reviews_per_listing = listings_reviews \
+  .groupBy(listings.id, listings.name) \
+  .agg(
+    F.count(reviews.id).alias('num_reviews')
+  ) \
+  .orderBy('num_reviews', ascending=False) \
+
+# Write the result to a .csv file
+reviews_per_listing \
+  .write \
+  .csv(args.output,header=True)
