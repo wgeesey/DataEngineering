eb3a5b0791a91ff6c7df25e721882352d5baf1d8
wgeesey <89809942+wgeesey@users.noreply.github.com>
2026-02-19 01:40:54 +0000

    Add files via upload

---
+++ MyFiles/MSSQL2.py
@@ -0,0 +1,84 @@
+from airflow.decorators import dag, task
+from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
+from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
+from airflow.operators.bash import BashOperator
+
+from datetime import datetime
+
+import pyodbc
+import os
+import csv
+SPARK_HOME = os.environ['SPARK_HOME']
+S3_BUCKET = "my-first-s3-data-lake"
+S3_RAW_KEY = "WWI/raw/orders/order_{{ ds_nodash }}.csv"
+#S3_PARQUET_KEY = "WWI/analytics/orders/order_{{ ds_nodash }}"
+
+@dag(
+    dag_id="mssql_extract_test2",
+    start_date=datetime(2025,1,1),
+    schedule_interval=None,
+    catchup=False,
+)
+def mssql_extract_dag2():
+
+    @task
+    def extract_order():
+        hook = MsSqlHook(
+            mssql_conn_id="mssql_wideworld",
+            schema="WideWorldImporters",
+            driver="pyodbc"
+        )
+
+        query = """
+        SELECT TOP 100 o.OrderID, o.OrderDate, c.CustomerName, 
+               ol.Quantity, ol.UnitPrice
+        FROM Sales.Orders o
+        LEFT JOIN Sales.OrderLines ol ON o.OrderID = ol.OrderID
+        LEFT JOIN Sales.Customers c ON o.CustomerID = c.CustomerID
+        """
+
+        records = hook.get_records(query)
+        AIRFLOW_HOME = os.environ["AIRFLOW_HOME"]
+        path = os.path.join(AIRFLOW_HOME, "data", "staging", "orders.csv")
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+        with open(path, "w", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([
+                "order_id", "order_date",
+                "customer_name", "quantity",
+                "unit_price"
+            ])
+            writer.writerows(records)
+
+        print(f"CSV created at {path}")
+        return path
+
+    extract_task = extract_order()
+    
+    upload_to_S3 = LocalFilesystemToS3Operator(
+        task_id="upload_csv_to_s3",
+        filename="{{ task_instance.xcom_pull(task_ids='extract_order') }}",
+        dest_key = S3_RAW_KEY,
+        dest_bucket = S3_BUCKET,
+        aws_conn_id="aws_default",
+        replace=True,
+    
+    )
+    
+    spark_transform = BashOperator(
+        task_id="csv_to_parquet",
+        bash_command="""
+        /opt/spark/bin/spark-submit \
+        --master local[*] \
+        --jars /opt/spark/jars/hadoop-aws-3.3.4.jar,/opt/spark/jars/aws-java-sdk-bundle-1.12.550.jar \
+        /home/wgeesey/airflow/dags/spark_jobs/transform_orders2.py \
+        --input_path s3a://my-first-s3-data-lake/WWI/raw/orders/order_{{ ds_nodash }}.csv \
+        --output_path s3a://my-first-s3-data-lake/WWI/analytics/orders/order_{{ ds_nodash }}
+        """
+    )
+
+    extract_task >> upload_to_S3 >> spark_transform
+
+dag = mssql_extract_dag2()
+
